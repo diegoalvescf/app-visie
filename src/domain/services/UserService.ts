88d5f8eb5457bcf1bebb05e7bbaadf7b55022886
diegoalvescf <dalvesdocs@gmail.com>
2023-06-06 13:58:54 -0300

    feat: adjust in services

--- src/domain/services/UserService.ts
+++ src/domain/services/UserService.ts
@@ -4,23 +4,23 @@ import _ from 'lodash';
 export class UserService {
   constructor(private userRepository: UserRepository) {}
 
-  getUsers(): Promise<IUser[]> {
-    return this.userRepository.getUsers();
+  async getUsers(): Promise<IUser[]> {
+    return await this.userRepository.getUsers();
   }
 
-  getUserById(id: IUser['_id']): Promise<IUser> {
-    return this.userRepository.getUserById(id);
+  async getUserById(id: IUser['_id']): Promise<IUser> {
+    return await this.userRepository.getUserById(id);
   }
 
-  createUser(user: IUserDTO): Promise<IUser> {
-    return this.userRepository.createUser(user);
+  async createUser(user: IUserDTO): Promise<IUser> {
+    return await this.userRepository.createUser(user);
   }
 
-  updateUser(id: IUser['_id'], user: IUser): Promise<IUser> {
-    return this.userRepository.updateUser(id, user);
+  async updateUser(id: IUser['_id'], user: IUser): Promise<IUser> {
+    return await this.userRepository.updateUser(id, user);
   }
 
-  deleteUser(id: IUser['_id']): Promise<void> {
-    return this.userRepository.deleteUser(id);
+  async deleteUser(id: IUser['_id']): Promise<void> {
+    await this.userRepository.deleteUser(id);
   }
 }
